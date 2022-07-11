e87b77539ab9b7afbf036b455c6cedb3a58577bd
rousong <recall131477@gmail.com>
2022-07-11 17:56:52 +0800

    deploy

---
+++ js/60.d408426a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue3_week7"]=self["webpackChunkvue3_week7"]||[]).push([[60],{5060:function(e,n,t){t.r(n),t.d(n,{default:function(){return k}});var c=t(6252);const r={class:"container"},s=(0,c._)("div",{class:"mt-4"},[(0,c._)("h2",{class:"text-center"},"關於我們")],-1),u=[s];function a(e,n){return(0,c.wg)(),(0,c.iD)("div",r,u)}var i=t(3744);const v={},f=(0,i.Z)(v,[["render",a]]);var k=f}}]);
+//# sourceMappingURL=60.d408426a.js.map
